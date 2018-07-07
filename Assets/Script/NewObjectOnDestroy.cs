7b5d762afc7ed0cbdcea43589dd2a9a8507e5512
zfw <zfw15@mails.tsinghua.edu.cn>
2018-07-07 17:39:26 +0800

    animation controller

---
+++ Assets/Script/NewObjectOnDestroy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewObjectOnDestroy : MonoBehaviour {
+    public GameObject destroyAnimation;
+    public GameObject[] newObjects;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnDestroy()
+    {
+        if (destroyAnimation != null)
+        {
+            Instantiate(destroyAnimation, gameObject.transform.position, Quaternion.identity);
+        }
+
+        foreach (GameObject go in newObjects)
+        {
+            Instantiate(go, gameObject.transform.position, Quaternion.identity);
+        }
+    }
+}
---
+++ Assets/Script/PlayerAnimation.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour {
+    public GameObject runAnimation;
+    public float animDelay;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(generateRunAnimation());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    private IEnumerator generateRunAnimation()
+    {
+        if (runAnimation != null)
+        {
+            while (true)
+            {
+                Vector3 moveDirection = getMoveDirection();
+                Debug.Log(moveDirection);
+                if (moveDirection != Vector3.zero)
+                {
+                    Quaternion rot = Quaternion.identity;
+                    rot.SetFromToRotation(Vector3.right, moveDirection);
+                    Instantiate(runAnimation, gameObject.transform.position, rot);
+                }
+                yield return new WaitForSeconds(animDelay);
+            }
+        }
+    }
+
+    private Vector3 getMoveDirection()
+    {
+        return gameObject.GetComponent<Player>().moveDirection;
+    }
+}

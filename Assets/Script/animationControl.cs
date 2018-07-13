2d293be50db3afcaf0536c911ef482e5bd1d022b
helloworld128 <1467491846@qq.com>
2018-07-13 12:29:11 +0800

    add 'Anima2D' which is essential for animations and a test scene

---
+++ Assets/Script/animationControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animationControl : MonoBehaviour {
+
+
+	private Animator anim;
+	private Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rb.AddForce(new Vector2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical")));
+		anim.SetFloat("m_speed",rb.velocity.magnitude);
+		
+	}
+}

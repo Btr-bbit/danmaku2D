a2a04fdc8a9450faa6a69a16681a05d47322b681
QinYue <747929791@qq.com>
2018-07-18 01:36:06 +0800

    gun list manager & number(1-5) change gun

--- Assets/Script/GunManager.cs
+++ Assets/Script/GunManager.cs
@@ -2,18 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class Gun
+{
+    public enum Type {
+        BasicGun,
+        BombGun,
+        FlameThrower,
+        ShotGun,
+        SniperGun
+    };
+    public Type type;//枪支名称(作为索引)
+    public GameObject emitter;//产生子弹的Emitter，需要在开枪时生成，停止开枪时销毁
+    public float clickInterval;//两次发射之间的最小时间间隔(鼠标两次点击间)
+    //以及一些控制枪支动画/模型的成员
+}
+
 public class GunManager : MonoBehaviour {
-    
-    public GameObject currentGun, basicGun;
+
+    static public GunManager instance;
+    public Gun[] guns;//设计的全部枪支列表
+    public List<int> ownedGuns;//玩家当前拥有的枪支ID
+    private Gun nowGun = null;//当前持握的枪支
+
+    public GameObject currentGun;
 
     // Use this for initialization
     void Start()
     {
-        
+        Debug.Assert(guns != null);
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Debug.LogWarning("Find more than one GunManager in scene!");
+            Destroy(this);
+        }
     }
 	
 	// Update is called once per frame
 	void Update () {
+        changeGun();
         processShoot();
 	}
 
@@ -25,7 +54,10 @@ public class GunManager : MonoBehaviour {
             {
                 Destroy(currentGun);
             }
-            currentGun = Instantiate(basicGun);
+            if (nowGun != null)
+                currentGun = Instantiate(nowGun.emitter);
+            else
+                Debug.Log("You are not equipped with a gun!");
             //currentGun.GetComponent<Emitter>().canFire = true;
         }
         else if (Input.GetMouseButtonUp(0))
@@ -35,10 +67,26 @@ public class GunManager : MonoBehaviour {
         }
         if (Input.GetMouseButton(0)) 
         {
-            currentGun.transform.position = gameObject.transform.position;
-            currentGun.transform.LookAt(Vector3.forward + gameObject.transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition) - gameObject.transform.position);
+            if (currentGun != null)
+            {
+                currentGun.transform.position = gameObject.transform.position;
+                currentGun.transform.LookAt(Vector3.forward + gameObject.transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition) - gameObject.transform.position);
+            }
         }
         //currentGun.transform.LookAt(Vector3.forward + gameObject.transform.position, Input.mousePosition() - gameObject.transform.position);
+    }
+
+    //数字键切枪
+    private void changeGun()
+    {
+        //判断用户是否按下上方数字键1-9，切枪
+        for(int i=1;i<=9;i++)
+        if (Input.GetKeyDown(KeyCode.Alpha0+i))
+        {
+                //列表里有此枪
+                if (ownedGuns.Count >= i)
+                    nowGun = guns[ownedGuns[i]];
+        }
     }
 
 }

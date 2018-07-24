67ec96f203c262ea094f74593c0c1f81429cf659
康鸿博 <hongbokang@icloud.com>
2018-07-24 12:31:41 +0800

    draw win/lose panel & add animation.

---
+++ Assets/Script/PanelButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void mouseClick() {
+        if (gameObject.name == "Restart") {
+            Debug.Log("restart");
+        } else if (gameObject.name == "Exit") {
+            Debug.Log("exit");
+        }
+    }
+}
---
+++ Assets/Script/PanelButtonAnim.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class PanelButtonAnim: MonoBehaviour {
+
+    private GameObject def, shine;
+    public float changeTime = 1.0f;
+    bool mouseOn = false;
+
+    void changeColor(GameObject @object, float speed)
+    {
+        Color col = @object.GetComponent<Image>().color;
+        float alpha = col.a;
+        alpha -= Time.deltaTime * speed;
+        if (alpha < 0.0f) alpha = 0.0f;
+        if (alpha > 1.0f) alpha = 1.0f;
+        col.a = alpha;
+        @object.GetComponent<Image>().color = col;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        def = transform.GetChild(0).gameObject;
+        shine = transform.GetChild(1).gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mouseOn)
+        {
+            changeColor(def, 1.0f / changeTime);
+            changeColor(shine, -1.0f / changeTime);
+        }
+        else
+        {
+            changeColor(def, -1.0f / changeTime);
+            changeColor(shine, 1.0f / changeTime);
+        }
+    }
+
+    public void mouseEnter()
+	{
+        mouseOn = true;
+	}
+
+    public void mouseExit()
+	{
+        mouseOn = false;
+	}
+}
